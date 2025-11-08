0bed3295fc954c24ac040f091118a6aefb30160a
nerellasudheer <nerellasudheer123@gmail.com>
2025-11-08 09:40:30 +0530

    added new code asking pet name
    ask pet name

--- test.py
+++ test.py
@@ -4,9 +4,12 @@ class welcome:
         
     def greet(self):
         print(f"Hello {self.name} , welcome")
+    def ask_pet(self):
+        print(f"So your pet name is {self.pet_name}")
 
 user_one=welcome("sudheer")
 user_one.greet()
+user_one.ask_pet()
 
 user_one=welcome("ram")
 user_one.greet()
